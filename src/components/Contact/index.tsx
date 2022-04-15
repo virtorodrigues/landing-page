04506b6dc18f1bb2f5c8d718bcdebddbf3d8b086
virtorodrigues <santos_vitao@hotmail.com>
2022-04-15 16:26:30 -0300

    create whatsapp redirect

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -48,7 +48,7 @@ export const Contact = () => {
       whileInView="onscreen"
       viewport={{ once: true, amount: 0.5 }}
       id='contact' className={styles.sectionContainer}>
-        
+
       <motion.h1 variants={titleVariants}>Contatos</motion.h1>
 
       <motion.div variants={cardVariants} className={styles.sectionContent}>
@@ -57,7 +57,7 @@ export const Contact = () => {
         <SocialContact icon={FaLinkedin} desctiption="Vitor Rodrigues" url="https://www.linkedin.com/in/virtorodrigues/" />
         <SocialContact icon={FaInstagramSquare} desctiption="virtorodrigues" url="https://www.instagram.com/virtorodrigues/" />
         <SocialContact icon={FaInstagramSquare} desctiption="reactjs.codetyps" url="https://www.instagram.com/reactjs.codetips/" />
-        <SocialContact icon={FaWhatsappSquare} desctiption="(18) 9 9718-3307" url="" />
+        <SocialContact icon={FaWhatsappSquare} desctiption="(18) 9 9718-3307" url="https://wa.me/5518997183307" />
       </motion.div>
     </motion.section>
   )
